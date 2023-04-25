9f5879d80191f002e2f9772724f6f9341f6d5043
Roberto M <robsan03@ucm.es>
2023-04-25 08:22:58 +0000

    Creamos fichero log Revisamos impresion del grafico.

--- dosemap.m
+++ dosemap.m
@@ -5,8 +5,8 @@
 
 function dosemap(rdsrFileName)
 
-% rdsr es el fichero que contiene la tabla con la información del informe 
-% estructurado de dosis.
+% rdsrFileName es el fichero que contiene la tabla de DOLQA con la 
+% información del informe estructurado de dosis.
 % Cada fila es un evento de radiación con los datos separados por ';' con
 % los siguientes campos:
 
@@ -107,50 +107,28 @@ function dosemap(rdsrFileName)
 % Mesa
 % https://dicom.nema.org/medical/Dicom/2016b/output/chtml/part03/sect_C.8.19.6.11.html
 
+%% Creamos fichero log con diary
+diary(strcat(rdsrFileName,'.log'));
+
 %% Lectura de los ficheros de eventos y almacenado en una tabla.
 try
     events = readtable(rdsrFileName, 'DecimalSeparator', ',');
 catch excepcion
     disp('error al abrir fichero');
+    disp(excepcion);
+    diary off;
     return
 end
 
 %% Inicializo la matriz de distribución de dosis
 m = 0;
-%% Calculamos los procedimientos y los eventos y los sumamos en un mismo
-%  fichero
-% Clasificamos por fabricante
-% switch events.Manufacturer{1}
-%     case 'Philips Medical Systems'
-%         DistFocoSuelo = 273; %mm
-%         %DistRPSuelo = 915; %mm
-%     case 'TOSHIBA_MEC'
-%         %DistRPSuelo = 890;
-%         DistFocoSuelo = 340;
-%         disp(strcat('Procedure',{' '},num2str(k),' performed at',{' '},...
-%         events.Manufacturer{1},' system.'));
-%         disp(strcat(events.Manufacturer(1),' not supported'));
-%         return %termina el programa;
-%     case 'SIEMENS'
-%         DistFocoSuelo = 310
-%         disp(strcat('Procedure',{' '},num2str(k),' performed at',{' '},...
-%         events.Manufacturer{1},' system.'));
-%         disp(strcat(events.Manufacturer(1),' not supported'));
-%         return %termina el programa;
-%     otherwise
-%         disp(strcat('Procedure',{' '},num2str(k),' performed at',{' '},...
-%         events.Manufacturer{1},' system.'));
-%         disp(strcat(events.Manufacturer(1),' not supported'));
-%         return %termina el programa;
-% end
 
 %% Clasificamos por region corporal
 switch events.TargetRegion{1}
     case 'Head'
-        disp(strcat('Procedure',{' '},num2str(k),' performed at',{' '},...
-        events.TargetRegion{1}));
         disp('Calculation NOT valid for Head');
-        disp('Program interrupted');
+        disp(events.TargetRegion);
+        diary off;
         return %termina el programa;
 end
 %% Revisamos colimación y utilizamos shutters si Collimated Field está
@@ -161,27 +139,24 @@ if any(events.CollimatedFieldWidth == 0) || any(events.CollimatedFieldHeight == 
 end
 % Si la colimación sigue siendo 0 termina programa
 if any(events.CollimatedFieldWidth == 0) || any(events.CollimatedFieldHeight == 0)
-    disp(strcat('Procedure',{' '},num2str(k),', event',{' '},num2str(i)));
     disp('Program terminated: No collimation information available');
+    diary off;
     return
 end
 
 %% Revisamos distancia foco isocentro
-if any(events.DistanceSourcetoIsocenter)==0
+if any(events.DistanceSourcetoIsocenter==0)
     disp('Program terminated: No distance source to isocenter available');
+    disp(events.DistanceSourcetoIsocenter);
+    diary off;
     return
 end
 
 %% Calculamos para cada evento
 nEvents = length(events.IrradiationEventUID);
 for i = 1:nEvents
-    AcquisPlane = events.AcquisitionPlane(i);
     primary_angle = events.PositionerPrimaryAngle(i);
     secondary_angle = events.PositionerSecondaryAngle(i);
-    shutter_left = events.LeftShutter(i);
-    shutter_rigth = events.RightShutter(i);
-    shutter_top = events.TopShutter(i);
-    shutter_bottom = events.BottomShutter(i);
     CollimatedFieldHeight = events.CollimatedFieldHeight(i)*...
         1000/events.DistanceSourcetoDetector(i);
     CollimatedFieldWidth = events.CollimatedFieldWidth(i)*...
@@ -195,13 +170,15 @@ for i = 1:nEvents
         couch_longitudinal = 0;
         couch_vertical = 0;
     else
-        couch_lateral = events.TableLongitudinalPosition(i)-events.TableLongitudinalPosition(i-1);
-        couch_longitudinal = events.TableLateralPosition(i)-events.TableLateralPosition(i-1);
+        couch_lateral = events.TableLongitudinalPosition(i)-...
+            events.TableLongitudinalPosition(i-1);
+        couch_longitudinal = events.TableLateralPosition(i)-...
+            events.TableLateralPosition(i-1);
+        % La coordenada vertical de la mesa depende del fabricante y en
+        % el caso de philips puede variar de una instalación a otra.
         couch_vertical = 0; % No utilizado events.TableHeightPosition(i)
     end
 
-    %HS = str2num(HeightSys);
-    %couch_vertical = events.TableHeightPosition(i)-(HS-150);
     kv = events.KVP(i);
     XRayFilter1Material = events.XRayFilter1Material(i);
     XRayFilter1ThicknessMinimum = events.XRayFilter1ThicknessMinimum(i);
@@ -216,10 +193,13 @@ for i = 1:nEvents
             XRayFilter2Material, XRayFilter2ThicknessMinimum);
     catch excepcion
         disp('error in dosemapevent function')
+        disp(excepcion);
+        diary off;
         return
     end
     m = m + dme;
 end
+
 % Pasamos a mGy para grafico.
 m = 1000*m;
 %% Obtenemos la dosis pico de la matriz suma de todos los eventos
@@ -230,15 +210,15 @@ psk = max(max(m));
 X=[-350 350];
 Y=[-350 350];
 fig = figure;
-%set(fig,'Visible','off'); % la figura no se ve pero se imprime
+set(fig,'Visible','off'); % la figura no se ve pero se imprime
 ax = axes;
 colormap jet
 c = ([100 300 1000 3000 10000 15000]);
 caxis = ([log(c(1)) log(c(length(c)))]);
-im = imagesc(X, Y, log(m+1), caxis);
+imagesc(X, Y, log(m+1), caxis);
 colorbar('FontSize',11,'YTick', log(c), 'YTickLabel', c);
 %ax.YDir = 'normal';
-daspect = [1,1];
+%daspect = [1,1];
 titul = strcat('Mapa de dosis a 15 cm bajo el isocentro. Dmax = ',...
     num2str(round(psk)),' mGy');
 ax.Title.String = titul;
@@ -247,26 +227,25 @@ ax.YLabel.String = '(mm)';
 text(-250,-425,'Warning: Lateral and longitudinal couch displacements were not available for dose calculation.',...
     'FontSize', 7);
 %% Escribimos resultados
-% Creamos nombre de fichero dependiendo de si calculamos uno o varios 
-% procedimienntos
-if length(procedures) == 1;
-    manymaps = nombrefichero;
-else
-    manymaps = strcat(nombrefichero,'_and_others');
-end
-NombreMapa = strcat(directorio,'\',manymaps,'.png');
+% Creamos nombre de fichero 
+NombreMapa = strcat(rdsrFileName,'.png');
 try
-    %print(fig, NombreMapa, '-dpng', '-r150');
     saveas(fig, NombreMapa, 'png');
-catch
+catch excepcion
     disp('error trying to write map');
+    disp(excepcion);
+    diary off;
     return
 end
-%clf(fig);
-%delete(get(fig,'children'));
-warning('off','all');
+clf(fig);
+delete(get(fig,'children'));
+%warning('off','all');
+
 % Proporciona el PSD por consola
 disp (psk);
+
+% Cierra el fichero log y el diario.
+diary off;
 return
 end
 
